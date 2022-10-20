4b1a83d2251cb9de240555957291b33f6454d873
Zarmons <fsantiagoovallos@gmail.com>
2022-10-20 17:28:55 -0500

    mejoras de mensajes respuesta

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ app = FastAPI()
 @app.post("/sms", name="SMS")
 def send_message(mobile_numbers: MobileNumbers):
     number = mobile_numbers.mobileNumbers
-    message = send_verify_number_phone(number)
-    return message
+    response = send_verify_number_phone(number)
+    return response
 
 #API para verificar codigo generado
 
 @app.post("/verification_code", name="VERIFICATION_CODE")
 def validate_code(verification_code: VerificationCode):
     code = verification_code.code
-    message = validate_verification_code(code)
-    return message
+    response = validate_verification_code(code)
+    return response
--- src/controller.py
+++ src/controller.py
@@ -6,22 +6,39 @@ import random
 # Validacion del numero celular y envio de SMS con en el codigo de validacion
 
 def send_verify_number_phone(number):
-    _REGEX_PHONE_NUMBER = re.compile(r'[(\+57|0057|57)?[ -]*(300|301|302|303|304|324|305|310|311|312|313|314|320|321|322|323|315|316|317|318|319|350|351|302|323|324|324|333|)[ -]*([0-9][ -]*){7}]')
+    _REGEX_PHONE_NUMBER = re.compile( r"[(\+57|0057|57)?[ -]*(300|301|302|303|304|324|305|310|311|312|313|314|320|321|322|323|315|316|317|318|319|350|351|302|323|324|324|333|)[ -]*([0-9][ -]*){7}]")
     if not _REGEX_PHONE_NUMBER.match(number):
         verification_code = create_verification_code()
-        url = 'https://api.reddantu.com/api/v2/SendSMS'
+        url = "https://api.reddantu.com/api/v2/SendSMS"
         data = {
-            'senderId': 'sms',
-            'message':  'su codigo de validacion es: ' f'{verification_code}',
-            'mobileNumbers': number,
-            'apiKey': 'Rq1vExJtdTs/oZuUzHm0vYQNGO3ifyoNke53wxlxUr4=',
-            'clientId': '64a8236c-ce6a-4f5a-abd1-a83ec221c7e7'
+            "senderId": "sms",
+            "message": "su codigo de validacion es: " f"{verification_code}",
+            "mobileNumbers": number,
+            "apiKey": "Rq1vExJtdTs/oZuUzHm0vYQNGO3ifyoNke53wxlxUr4=",
+            "clientId": "64a8236c-ce6a-4f5a-abd1-a83ec221c7e7",
         }
-        response = requests.post(url, json=data)
-        message = 'El codigo fue enviado correctamente al numero de celular: ' f'{number}'
-    else :
-        message = 'Por favor verifique su numero de celular'
-    return message
+        response_sms = requests.post(url, json=data)
+        response = {
+            "Data": [
+                {
+                    "MessageDescription": "success",
+                    "Message": "El codigo fue enviado correctamente al numero de celular: "
+                    f"{number}",
+                    "MobileNumber": {number},
+                }
+            ]
+        }
+    else:
+        response = {
+            "Data": [
+                {
+                    "MessageDescription": "error",
+                    "Message": "Por favor verifique su numero de celular",
+                    "MobileNumber": {number},
+                }
+            ]
+        }
+    return response
 
 # Creacion de codigos de seis digitos aleatoriamente
 
@@ -32,10 +49,8 @@ def create_verification_code():
 # Validacion para saber si el codigo ingresado si es igual al generado
 
 def validate_verification_code(code):
-    if code == "123456" :
-        message = 'codigo valido'
-    else :
-        message = 'verifica tu codigo'
-    return message
-
-
+    if code == "123456":
+        response = "codigo valido"
+    else:
+        response = "verifica tu codigo"
+    return response
