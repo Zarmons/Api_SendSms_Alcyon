7a5946cf70d4fbaa62061c6f2ae38bab4c62951c
Zarmons <fsantiagoovallos@gmail.com>
2022-10-26 14:51:36 -0500

    mejora de la expresion regular para validar numeros telefonicos

--- src/controller.py
+++ src/controller.py
@@ -5,10 +5,11 @@ global global_code
 # Validacion del numero celular y envio de SMS con en el codigo de validacion
 
 def send_verify_number_phone(number):
-    _REGEX_PHONE_NUMBER = re.compile(
-        r"[(\+57|0057|57)?[ -]*(300|301|302|303|304|324|305|310|311|312|313|314|320|321|322|323|315|316|317|318|319|350|351|302|323|324|324|333|)[ -]*([0-9][ -]*){7}]"
-    )
-    if not _REGEX_PHONE_NUMBER.match(number):
+    regex = r"^(57)\s?(300|301|302|303|304|324|305|310|311|312|313|314|320|321|322|323|315|316|317|318|319|350|351|302|323|324|324|333)\s?([0-9]){7}$"
+    result = re.match(regex, number)
+    if result is None:
+        response = response_data( "messageNumber", "error", "Por favor verifique su numero de celular", f"{number}", "null" )
+    else:
         verification_code = create_verification_code()
         url = "https://api.reddantu.com/api/v2/SendSMS"
         data = {
@@ -20,8 +21,6 @@ def send_verify_number_phone(number):
         }        
         response_sms = requests.post(url, json=data)
         response = response_data( "messageNumber", "success", "El codigo fue enviado correctamente al numero de celular: " f"{number}", f"{number}", "null" )
-    else:
-        response = response_data( "messageNumber", "error", "Por favor verifique su numero de celular", f"{number}", "null" )
     return response
 
 # Creacion de codigos de seis digitos aleatoriamente
